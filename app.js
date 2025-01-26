5b40f78a723afe022a7aa6043fd884bdf6adb046
karnbhushan1994 <karn.becs.13@gmail.com>
2025-01-26 19:40:43 +0530

    ignitOurApp

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-// Manipulate the HTML DOM using Javscript
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Namaste Everyone";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
+// ## Namaste React Course by Akshay Saini
+// # Chapter 02 - Igniting our App
 
-// Manipulate the HTML DOM using React
+/*
+ *** Parcel Feature ***
+ * Created A Server
+ * HMR - Hot Module Replacement
+ * File Watcher algorithm - C++ (Execute File when changes occur)
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code (Example - Remove Console.log)
+ * Dev and Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatible with older version of browser
+ * HTTPS on dev [Example - npx parcel index.html --https]
+ * Port Number [Example - If port number using in localhost then it will change in port number in another project running on localhost]
+ * Consistent Hashing Algorithm
+ * Zero Config
+ * Tree shaking
+ *
+ *
+ * Transitive Dependencies
+ */
 
-// Create nested React Elements
+
+// imported react and reactdom from nodemodule folder
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const heading = React.createElement(
     "h1",
     {
       id: "title",
-      style: {
-        background:"red",
-      },
-      className:"title"
     },
     "heading"
   );
